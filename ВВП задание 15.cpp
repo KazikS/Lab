f0e7859f2eab7313b3f8ba588665c11fec6192aa
KazikS <90966310+KazikS@users.noreply.github.com>
2022-01-15 15:38:55 +0300

    Add files via upload

---
+++ ВВП задание 15.cpp
@@ -0,0 +1,74 @@
+﻿#include <iostream>
+using namespace std;
+void powerA3(double a, double* b) {
+	*b = a * a * a;
+}
+int Sign(double x) {
+	if (x < 0) {
+		return -1;
+	}
+	else if (x == 0) {
+		return 0;
+	}
+	else if (x > 1) {
+		return 1;
+	}
+}float RingS(float r1, float r2) {
+	if (r1 > r2) {
+		float S = 3.14 * (r1 * r1 - r2 * r2);
+		return S;
+	}
+	else if (r2 > r1) {
+		float S = 3.14 * (r2 * r2 - r1 * r1);
+		return S;
+	}
+}
+int Quarter(int x, int y) {
+	if (x > 0 && y > 0) return 1;
+	else if (x > 0 && y < 0) return 4;
+	else if (x < 0 && y < 0) return 3;
+	else if (x < 0 && y > 0) return 2;
+}
+double fact2(int N) {
+	double ff = 1;
+	for (; N > 1; N -= 2)
+		ff *= N;
+	return ff;
+}
+
+int main(void) {
+	setlocale(LC_ALL, "rus");
+	cout << "Задание 1\n Введите число А \n";
+	for (int i = 0; i < 5; i++) {
+		double a;
+		cin >> a;
+		double b;
+		powerA3(a, &b);
+
+		cout << a << " ^ 3 = " << b << endl;
+	}
+
+	cout << "Задание 2" << endl;
+	cout << "Введите числa " << endl;
+	int n, h;
+	cin >> n >> h;
+	cout << Sign(n) + Sign(h) << endl;
+
+	cout << "Задание 3" << endl;
+	cout << "Введите радиусы окружностей " << endl;
+	float r1, r2;
+	cin >> r1 >> r2;
+	cout << "Площадь кольца между окружностями равна " << RingS(r1, r2) << endl;
+
+	cout << "Задание 4" << endl;
+	cout << "Введите координаты точки для определния ее местоположения " << endl;
+	int x, y;
+	cin >> x >> y;
+	cout << "Точка находится в " << Quarter(x, y) << " четверти" << endl;
+
+	cout << "Задание 5" << endl;
+	cout << "Введите число для определения двойного факториала " << endl;
+	int f;
+	cin >> f;
+	cout << fact2(f);
+}
---
+++ ввп задание 14.cpp
@@ -0,0 +1,105 @@
+﻿#include <iostream>
+using namespace std;
+
+int fib(int n) {
+    int f = 0;
+    int res = 1;
+    int f0 = 1;
+    int i = 0;
+    while (i <= n) {
+        res = f;
+        f = f0 + f;
+        f0 = res;
+        i++;
+        if (n == f)
+        {
+            cout << "номер: ";
+            return i;
+        }
+    }
+    if (n == 0)
+    {
+
+        return 0;
+    }
+    else
+    {
+        cout << "ошибка ";
+        return 404;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    cout << "задание 1" << endl;
+    int a, b;
+    cout << "Номер 1\n";
+    cout << "Введите a и b ";
+    cin >> a >> b;
+    int i = a;
+    cout << endl;
+    for (i; i <= b; i++)
+    {
+        cout << endl;
+        for (int j = 0; j < i; j++)
+        {
+            cout << i << " ";
+        }
+    }
+
+    cout << endl;
+    cout << "номер 2" << endl;
+    cout << "Введите числа a и b: " << endl;
+    cin >> a >> b;
+    i = 0;
+    int g = a;
+    while (a > b) {
+        a -= b;
+        i++;
+    }
+    cout << g - b * i << endl;
+
+    cout << "задание 3" << endl;
+    int N, k;
+    cout << "введите N:\n";
+    cin >> N;
+    k = 1;
+    i = 1;
+    while (i < N)
+    {
+        k += 1;
+        i += k;
+    }
+    cout << "Наименьшее к = " << k << endl << "Сумма равна: " << i << endl;
+    cout << "\n__________________________\n";
+
+    cout << "задание 4" << endl;
+    cout << "введие процент вклада (от 0 до 25) " << endl;
+    double p;
+    cin >> p;
+    double x = 1000;
+    int i = 0;
+    while (x <= 1100) {
+        x += p / 100 * x;
+        i++;
+    }
+    cout << i << " месяца \n" << "сумма " << x << endl;
+
+    cout << "задание 5\n";
+    long int A, B;
+    cout << "Введите 2 числа: ";
+    cin >> A >> B;
+    while (A != B) {
+        if (A > B)
+            A = A - B;
+        else
+            B = B - A;
+    }
+    cout << "НОД этих чисел: " << A << endl;
+
+    cout << "задание 6";
+    cout << "\n введите число Фибоначчи для определения его порядкового номера ";
+    int f;
+    cin >> f;
+    cout << fib(f);
+}
